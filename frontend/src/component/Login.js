5ce5a55c189b32487bfdb3c900499203b1a017ca
m-triple-m <triplem656@gmail.com>
2022-08-08 19:50:46 +0530

    ..

--- frontend/src/component/Login.js
+++ frontend/src/component/Login.js
@@ -1,102 +1,87 @@
-
-
-
-
-import { Formik } from 'formik';
-import React from 'react';
-import Swal from 'sweetalert2';
-import { NavLink } from 'react-router-dom';
+import { Formik } from "formik"
+import React from "react"
+import Swal from "sweetalert2"
+import { NavLink, useNavigate } from "react-router-dom"
 // import './login.css';
 
 const Login = () => {
 
-    const userSubmit =  async (formdata) => {
-        console.log(formdata);
+  const navigate = useNavigate();
 
-        // fetch - url,request method, data, data format
+  const userSubmit = async (formdata) => {
+    console.log(formdata)
 
-       const res= await fetch('http://localhost:5000/user/authenticate',
-        {method: 'POST',
-      body : JSON.stringify(formdata),
-      headers : {
-        'Content-Type' : 'application/json'
-      }
+    // fetch - url,request method, data, data format
+
+    const res = await fetch("http://localhost:5000/user/authenticate", {
+      method: "POST",
+      body: JSON.stringify(formdata),
+      headers: {
+        "Content-Type": "application/json",
+      },
     })
 
     Swal.fire({
-  title: 'Enter your name',
-  input: 'text',
-  customClass: {
-    validationMessage: 'my-validation-message'
-  },
-  preConfirm: (value) => {
-    if (!value) {
-      Swal.showValidationMessage(
-        '<i class="fa fa-info-circle"></i> Your name is required'
-      )
+      title: "Enter your name",
+      input: "text",
+      customClass: {
+        validationMessage: "my-validation-message",
+      },
+      preConfirm: (value) => {
+        if (!value) {
+          Swal.showValidationMessage('<i class="fa fa-info-circle"></i> Your name is required')
+        }
+      },
+    })
+
+    if (res.status === 200) {
+      Swal.fire({
+        icon: "success",
+        title: "login successful",
+        text: "You are Logged In",
+      })
+      navigate("/ds")
+    } else if (res.status === 400) {
+      Swal.fire({
+        icon: "error",
+        title: "login failed",
+        text: "Invalid username or password",
+      })
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "login faild",
+        text: "Something went wrong",
+      })
     }
   }
-})
 
-    if(res.status===200)
-    {  Swal.fire({
-      icon : 'success',
-      title : 'login successful',
-       text : 'You are Logged In'
-    })
-  }else if(res.status===400){ Swal.fire({
-    icon : 'error',
-    title : 'login failed',
-     text : 'Invalid username or password'
-  })
-    }
+  return (
+    <div className="mycard">
+      <div className="col-md-4 mx-auto bg-light">
+        <div className="card mt-5">
+          <div className="card-body">
+            <Formik initialValues={{ email: "", password: "" }} onSubmit={userSubmit}>
+              {({ values, handleChange, handleSubmit }) => (
+                <form onSubmit={handleSubmit}>
+                  <h3 className="text-center">Sign-in Here</h3>
+                  <hr />
 
-    else{
-      Swal.fire({
-        icon : 'error',
-        title : 'login faild',
-         text : 'Something went wrong'
-      })
-       
-    }
-    
-    }
+                  <label className="mt-4">Email</label>
+                  <input value={values.email} onChange={handleChange} id="email" className="form-control" />
 
-    return (
-        <div className="mycard">
-            <div className="col-md-4 mx-auto bg-light">
-                <div className="card mt-5">
-                    <div className="card-body">
+                  <label className="mt-4">Password</label>
+                  <input value={values.password} onChange={handleChange} id="password" type="password" className="form-control" />
 
-                        <Formik initialValues={{email : '', password : ''}} onSubmit={userSubmit}>
-                            {({values, handleChange, handleSubmit}) => (
-                                <form onSubmit={handleSubmit}>
-                                    
-                                    <h3 className="text-center">Sign-in Here</h3>
-                                    <hr />
-
-                                    <label className='mt-4'>Email</label>
-                                    <input value={values.email} onChange={handleChange} id="email" className='form-control' />
-                                    
-                                    <label className='mt-4'>Password</label>
-                                    <input value={values.password} onChange={handleChange} id="password" type="password" className='form-control' />
-                                
-                                    <NavLink className="nav-link" to="/ds">
-                                    <button className='btn btn-secondary mt-5'>Login Now</button>
-          </NavLink>
-     
-                                   
-                                    
-
-                                </form>
-                            )}
-                        </Formik>
-
-                    </div>
-                </div>
-            </div>
+                    <button className="btn btn-secondary mt-5">Login Now</button>
+                </form>
+              )}
+            </Formik>
+          </div>
         </div>
+      </div>
+    </div>
   )
 }
 
-export default Login;
+export default Login
