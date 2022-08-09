0919f07e9cd9e7befd7534847db48082b1aeef50
Mariyam2724 <85488366+Mariyam2724@users.noreply.github.com>
2022-08-09 19:31:00 +0530

    ..

--- frontend/src/component/Dashboard2.js
+++ frontend/src/component/Dashboard2.js
@@ -2,7 +2,7 @@
 /* global google */
 
 import React, { useEffect, useState } from 'react';
-import {AppBar, Box, Tab, Tabs} from '@mui/material';
+// import {AppBar, Box, Tab, Tabs} from '@mui/material';
 
 
 const Dashboard2 = () => {
